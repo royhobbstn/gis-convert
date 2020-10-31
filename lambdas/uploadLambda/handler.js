75e2fae27dc35887a5069ffdbc068a6314060b17
Daniel Trone <danieljtrone@gmail.com>
2020-10-31 06:13:54 -0500

    in progress lambda

---
+++ lambdas/uploadLambda/handler.js
@@ -0,0 +1,51 @@
+//fileUploaderHome.js
+'use strict';
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v4');
+const s3 = new AWS.S3();
+const formParser = require('./formParser');
+
+const bucket = process.env.Bucket;
+const MAX_SIZE = 10000000; // ~10MB
+
+const uploadToS3 = (bucket, key, buffer, mimeType) =>
+  new Promise((resolve, reject) => {
+    s3.upload({ Bucket: bucket, Key: key, Body: buffer, ContentType: mimeType }, function (
+      err,
+      data,
+    ) {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+
+module.exports.uploader = async event => {
+  try {
+    const formData = await formParser.parser(event, MAX_SIZE);
+    const file = formData.files[0];
+
+    const uid = uuid();
+    const originalKey = `${uid}_original_${file.filename}`;
+    const originalFile = await uploadToS3(bucket, originalKey, file.content, file.contentType);
+
+    const signedOriginalUrl = s3.getSignedUrl('getObject', {
+      Bucket: originalFile.Bucket,
+      Key: originalKey,
+      Expires: 60000,
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        id: uid,
+        mimeType: file.contentType,
+        originalKey: originalFile.key,
+        bucket: originalFile.Bucket,
+        fileName: file.filename,
+        originalUrl: signedOriginalUrl,
+      }),
+    };
+  } catch (e) {
+    return { statusCode: 500, body: JSON.stringify(e.message) };
+  }
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+
+import { Button } from 'semantic-ui-react';
+
 import './App.css';
 
 function App() {
+  const fileUpload = useRef(null);
+
+  function uploadFile(evt) {
+    let file = evt.target.files[0];
+    console.log(file);
+    if (!file || !file.name) {
+      window.alert('Please select a file');
+      return false;
+    }
+    if (file.size > 10e6) {
+      window.alert('Please upload a file smaller than 10 MB');
+      return false;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: '10%',
+          width: '220px',
+          display: 'block',
+        }}
+        primary
+        content="Choose File to Upload"
+        labelPosition="left"
+        icon="file"
+        onClick={() => fileUpload.current.click()}
+      />
+
+      <input ref={fileUpload} type="file" hidden onChange={uploadFile} />
     </div>
   );
 }
