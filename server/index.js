6b67d65266cd50f829be43de0ccc5b9fbc6374e5
Daniel Trone <danieljtrone@gmail.com>
2020-11-17 06:49:51 -0600

    redirect http to https on prod

--- server/index.js
+++ server/index.js
@@ -40,6 +40,18 @@ app.use(bodyParser.json());
 app.use(express.static('build'));
 app.use(helmet());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(function (req, res, next) {
+    if (req.secure) {
+      // request was via https, so do no special handling
+      next();
+    } else {
+      // request was via http, so redirect to https
+      res.redirect('https://' + req.headers.host + req.url);
+    }
+  });
+}
+
 createDirectories([tempFolder, logsFolder]);
 
 // set up worker
